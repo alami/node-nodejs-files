270a787a547330670fb19ac5504125c1cc8cf062
aminov <alami@tshtt.uz>
2023-04-21 11:45:27 +0500

    trim,split cmd

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,12 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+export const usersOSdir='c:\\Users\\'
+
 export const getCurrentFolder = (path) => {
     const filename = fileURLToPath(path);
     return dirname(filename);
 }
 
-export const msgErrInput = 'Invalid input, ';
-export const msgErrStart = 'use: node start -- --username="username"';
-export const msgWelcome = 'Welcome to the File Manager, ';
+export const msgErrInput = 'Invalid input';
+export const msgErrStart = ', use: node start -- --username="username"';
